56958a5ed2d740a8af65f66ca6f05693d9c13deb
justcomex <justcomex@gmail.com>
2019-05-09 21:10:43 +0700

    Add Rosenbrock method with Golden Section optimization

---
+++ golden_section_method.py
@@ -0,0 +1,62 @@
+import math
+
+
+# Метод золотого сечения для одномерного поиска
+def goldenSection(function, l0, X, S, EPS):
+    interval = searchSection(function, l0, X, S)
+    if interval[1] >= interval[0]:
+        b = interval[1]
+        a = interval[0]
+    else:
+        a = interval[1]
+        b = interval[0]
+    lenOfSection = b - a
+    l1 = a + (3 - math.sqrt(5)) / 2 * (b - a)
+    l2 = a + (math.sqrt(5) - 1) / 2 * (b - a)
+    f1 = function(X[0] + l1 * S[0], X[1] + l1 * S[1])
+    f2 = function(X[0] + l2 * S[0], X[1] + l2 * S[1])
+    while lenOfSection > EPS:
+        if f1 > f2:
+            a = l1
+            l1 = l2
+            l2 = a + (math.sqrt(5) - 1) / 2 * (b - a)
+            f1 = f2
+            f2 = function(X[0] + l2 * S[0], X[1] + l2 * S[1])
+        else:
+            b = l2
+            l2 = l1
+            l1 = a + (3 - math.sqrt(5)) / 2 * (b - a)
+            f2 = f1
+            f1 = function(X[0] + l1 * S[0], X[1] + l1 * S[1])
+        lenOfSection = b - a
+    return (b + a) / 2
+
+# Поиск отрезка, содержащий минимум 
+def searchSection(function, l0, X, S):
+    delta = 0.001
+    l1 = 0
+    h = 0
+    f0 = function(X[0] + l0 * S[0], X[1] + l0 * S[1])
+    if f0 > function(X[0] + (l0 + delta) * S[0], X[1] + (l0 + delta) * S[1]):
+        l1 = l0 + delta
+        h = delta
+    elif f0 > function(X[0] + (l0 - delta) * S[0], X[1] + (l0 - delta) * S[1]):
+        l1 = l0 - delta
+        h = - delta
+    else:
+        return l0 - delta, l0 + delta
+    h = 2 * h
+    f1 = function(X[0] + l1 * S[0], X[1] + l1 * S[1])
+    l2 = l1 + h
+    f2 = function(X[0] + l2 * S[0], X[1] + l2 * S[1])
+    iter = 0
+    while f1 > f2:
+        l0 = l1
+        f0 = f1
+        l1 = l2
+        f1 = f2
+        h = 2 * h
+        l2 = l1 + h
+        f2 = function(X[0] + l2 * S[0], X[1] + l2 * S[1])
+        iter = iter + 1
+    return l0 - h / 2, l2
---
+++ rosenbrock_method.py
@@ -0,0 +1,60 @@
+import copy
+import math
+
+from golden_section_method import *
+
+
+# Вычисление нормы
+def findNorm(V):
+    return math.sqrt(V[0] ** 2 + V[1] ** 2)
+
+# Условие выхода
+def exitCondition(function, X1, X0, EPS):
+    if math.fabs(function(X1[0], X1[1]) - function(X0[0], X0[1])) < EPS:
+        return True
+    elif math.fabs(X1[0] - X0[0]) < EPS and math.fabs(X1[1] - X0[1]) < EPS:
+        return True
+    return False
+
+# Метод Розенброка
+def RosenbrockMethod(function, X, S1, S2, EPS):
+    A = [[0, 0], [0, 0]]
+    B = [0, 0]
+    iter = 0
+    while True:
+        X0 = copy.copy(X)
+
+        L1 = goldenSection(function, -1, X, S1, EPS)
+        X[0] = X0[0] + L1 * S1[0]
+        X[1] = X0[1] + L1 * S1[1]
+
+        L2 = goldenSection(function, 0, X, S2, EPS)
+        X[0] = X[0] + L2 * S2[0]
+        X[1] = X[1] + L2 * S2[1]
+
+        A[0][0] = L1 * S1[0] + L2 * S2[0]
+        A[0][1] = L1 * S1[1] + L2 * S2[1]
+
+        if math.fabs(L1) >= math.fabs(L2):
+            A[1][0] = L2 * S2[0]
+            A[1][1] = L2 * S2[1]
+        else:
+            A[1][0] = L1 * S2[0]
+            A[1][1] = L1 * S2[1]
+
+        S1[0] = A[0][0] / findNorm(A[0])
+        S1[1] = A[0][1] / findNorm(A[0])
+
+        K = A[1][0] * S1[0] + A[1][1] * S1[1]
+        B[0] = A[1][0] - K * S1[0]
+        B[1] = A[1][1] - K * S1[1]
+
+        S2[0] = B[0] / findNorm(B)
+        S2[1] = B[1] / findNorm(B)
+        iter = iter + 1
+
+        func = function(X[0], X[1])
+
+        if exitCondition(function, X, X0, EPS):
+            break
+    return X
